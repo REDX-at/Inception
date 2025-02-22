46651477723c8cae4025b8e630a2b1fd0196f2a9
Redx-at <taoussi.aimen@gmail.com>
2025-02-22 17:01:19 +0100

    test

--- src/wordpress/wp.sh
+++ src/wordpress/wp.sh
@@ -3,9 +3,10 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root
 # wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$(cat run/secrets/db_password) --dbhost=$DB_HOST --path=/var/www/html --allow-root
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path=/var/www/html --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --path=/var/www/html --allow-root
+# wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASSWORD --admin_email=$WP_EMAIL --path=/var/www/html --allow-root
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$(cat run/secrets/wp_password) --admin_email=$WP_EMAIL --path=/var/www/html --allow-root
 wp user create $SECONDARY_USER $SECONDARY_MAIL --role=$SECONDARY_ROLE --user_pass=$SECONDARY_PASSWORD --allow-root
 
 php-fpm7.4 -F
