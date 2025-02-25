8988ec52e8fce1a2e318eb99a15cd272ab446607
Redx-at <taoussi.aimen@gmail.com>
2025-02-25 15:52:55 +0000

    redis set up Done

--- srcs/requirements/wordpress/wp.sh
+++ srcs/requirements/wordpress/wp.sh
@@ -3,9 +3,19 @@
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$(cat /run/secrets/db_password) --dbhost=$DB_HOST --path=/var/www/html --allow-root
-wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$(cat /run/secrets/wp_password) --admin_email=$WP_EMAIL --path=/var/www/html --allow-root
-wp user create $USER $MAIL --role=$ROLE --user_pass=$(cat /run/secrets/user_password) --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
+wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --path=/var/www/html --allow-root
+wp user create $USER $MAIL --role=$ROLE --user_pass=$PASS --allow-root
 
+wp plugin install redis-cache --activate --allow-root
+
+wp config set WP_REDIS_HOST 'redis' --allow-root
+wp config set WP_CACHE 'true' --allow-root
+wp config set FS_METHOD 'direct' --allow-root
+wp config set WP_REDIS_PORT '6379' --allow-root
+
+wp redis enable --allow-root
+
+chmod -R o+w /var/www/html/ 
 
 php-fpm7.4 -F
