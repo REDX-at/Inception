9b329132b3dcb5153d57baf6975fb6d03e6fbaf2
Redx-at <taoussi.aimen@gmail.com>
2025-02-26 16:59:01 +0000

    Script modified in srcs/requirements/bonus/ftp/ftp.sh

--- srcs/requirements/bonus/ftp/ftp.sh
+++ srcs/requirements/bonus/ftp/ftp.sh
@@ -1,13 +1,3 @@
-# #!/bin/bash
-
-# useradd -m -d /var/www/html -s /bin/bash ftpuser && echo "ftpuser:pass" | chpasswd
-
-# chown -R ftpuser:ftpuser /var/www/html
-# chmod -R 755 /var/www/html
-
-# mkdir -p /var/run/vsftpd/empty
-
-# vsftpd /etc/vsftpd.conf
 #!/bin/bash
 
 useradd -m -d /var/www/html -s /bin/bash "$FTP_USER" && echo "$FTP_USER:$FTP_PASS" | chpasswd
