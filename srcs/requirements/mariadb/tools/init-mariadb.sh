c558d786a2919ff4aa5f93d929dd1e8b1bdf444c
Redx-at <taoussi.aimen@gmail.com>
2025-03-14 15:36:16 +0000

    Modif the gitignore and the mariadb script

--- srcs/requirements/mariadb/tools/init-mariadb.sh
+++ srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,38 +1,17 @@
 # !/bin/bash
 
-# service mariadb start
-
-# sleep 10
-
-# mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
-# mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-# mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-# # mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${ROOT_PASS}';"
-# # mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
-# # mysql -u root -p$ROOT_PASS -e "FLUSH PRIVILEGES;"
-# mariadb -e "FLUSH PRIVILEGES;"
-
-# # mysqladmin -u root -p$ROOT_PASS shutdown
-# service mariadb stop
-
-# echo "MariaDB is running successfully ..."
-
-# exec mysqld_safe --bind-address=0.0.0.0
-
 service mariadb start
 
-sleep 5
+mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
+mariadb -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER';"
+mariadb -e "FLUSH PRIVILEGES;"
 
-mysql -u root --skip-password -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
-mysql -u root --skip-password -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
-mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
-mysql -u root --skip-password -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${ROOT_PASS}';"
-mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';"
-mysql -u root -p$ROOT_PASS -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASS}';" 
 mysqladmin -u root -p$ROOT_PASS shutdown
 
 service mariadb stop
 
 echo "MariaDB is running successfully ..."
 
-exec mysqld_safe --bind-address=0.0.0.0
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
