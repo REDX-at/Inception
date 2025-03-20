7aaa4d76a8b89760dc7d7cffe0e766d35c4301f7
Redx-at <taoussi.aimen@gmail.com>
2025-03-19 20:27:36 -0400

    From the kali linux add sleep to the script : wordpress && mariadb

--- srcs/requirements/mariadb/tools/init-mariadb.sh
+++ srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -2,6 +2,8 @@
 
 service mariadb start
 
+sleep 5
+
 mariadb -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME};"
 mariadb -e "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';"
 mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER';"
